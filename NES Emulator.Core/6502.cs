32f312760251ae0e7591859826ba41404463d514
superuserC <kevin.danthine@gmail.com>
2021-06-24 00:02:20 +0200

    Starting implementation of processor instruction set / addressing mode mapping

--- NES Emulator.Core/6502.cs
+++ NES Emulator.Core/6502.cs
@@ -26,6 +26,11 @@ namespace NES_Emulator.Core
     /// </summary>
     public class _6502
     {
+        public _6502()
+        {
+            InstructionsMap = MapProcessorInstructions();
+        }
+
         /// <summary>
         /// Accumulator
         /// </summary>
@@ -48,6 +53,8 @@ namespace NES_Emulator.Core
         /// </summary>
         public byte Status_Register { get; set; } = 0x00;
 
+        public List<Instruction> InstructionsMap { get; set; }
+
         #region events
         /// <summary>
         /// See processor sheet.
@@ -68,6 +75,94 @@ namespace NES_Emulator.Core
         /// See processor sheet.
         /// </summary>
         public void Reset() { }
+
+        #endregion
+
+        #region addressing mode
+
+        public byte AM_IMP() { throw new NotImplementedException(); }
+        public byte AM_ZP0() { throw new NotImplementedException(); }
+        public byte AM_ZPY() { throw new NotImplementedException(); }
+        public byte AM_ABS() { throw new NotImplementedException(); }
+        public byte AM_ABY() { throw new NotImplementedException(); }
+        public byte AM_IZX() { throw new NotImplementedException(); }
+        public byte AM_IMM() { throw new NotImplementedException(); }
+        public byte AM_ZPX() { throw new NotImplementedException(); }
+        public byte AM_REL() { throw new NotImplementedException(); }
+        public byte AM_ABX() { throw new NotImplementedException(); }
+        public byte AM_IND() { throw new NotImplementedException(); }
+        public byte AM_IZY() { throw new NotImplementedException(); }
+        public byte AM_XXX() { throw new NotImplementedException(); }
+
+        #endregion
+
+        #region instructions
+
+        public byte ADC() { throw new NotImplementedException(); }
+        public byte BCS() { throw new NotImplementedException(); }
+        public byte BNE() { throw new NotImplementedException(); }
+        public byte BVS() { throw new NotImplementedException(); }
+        public byte CLV() { throw new NotImplementedException(); }
+        public byte DEC() { throw new NotImplementedException(); }
+        public byte INC() { throw new NotImplementedException(); }
+        public byte JSR() { throw new NotImplementedException(); }
+        public byte LSR() { throw new NotImplementedException(); }
+        public byte PHP() { throw new NotImplementedException(); }
+        public byte ROR() { throw new NotImplementedException(); }
+        public byte SEC() { throw new NotImplementedException(); }
+        public byte STX() { throw new NotImplementedException(); }
+        public byte TSX() { throw new NotImplementedException(); }
+        public byte AND() { throw new NotImplementedException(); }
+        public byte BEQ() { throw new NotImplementedException(); }
+        public byte BPL() { throw new NotImplementedException(); }
+        public byte CLC() { throw new NotImplementedException(); }
+        public byte CMP() { throw new NotImplementedException(); }
+        public byte DEX() { throw new NotImplementedException(); }
+        public byte INX() { throw new NotImplementedException(); }
+        public byte LDA() { throw new NotImplementedException(); }
+        public byte NOP() { throw new NotImplementedException(); }
+        public byte PLA() { throw new NotImplementedException(); }
+        public byte RTI() { throw new NotImplementedException(); }
+        public byte SED() { throw new NotImplementedException(); }
+        public byte STY() { throw new NotImplementedException(); }
+        public byte TXA() { throw new NotImplementedException(); }
+        public byte ASL() { throw new NotImplementedException(); }
+        public byte BIT() { throw new NotImplementedException(); }
+        public byte BRK() { throw new NotImplementedException(); }
+        public byte CLD() { throw new NotImplementedException(); }
+        public byte CPX() { throw new NotImplementedException(); }
+        public byte DEY() { throw new NotImplementedException(); }
+        public byte INY() { throw new NotImplementedException(); }
+        public byte LDX() { throw new NotImplementedException(); }
+        public byte ORA() { throw new NotImplementedException(); }
+        public byte PLP() { throw new NotImplementedException(); }
+        public byte RTS() { throw new NotImplementedException(); }
+        public byte SEI() { throw new NotImplementedException(); }
+        public byte TAX() { throw new NotImplementedException(); }
+        public byte TXS() { throw new NotImplementedException(); }
+        public byte BCC() { throw new NotImplementedException(); }
+        public byte BMI() { throw new NotImplementedException(); }
+        public byte BVC() { throw new NotImplementedException(); }
+        public byte CLI() { throw new NotImplementedException(); }
+        public byte CPY() { throw new NotImplementedException(); }
+        public byte EOR() { throw new NotImplementedException(); }
+        public byte JMP() { throw new NotImplementedException(); }
+        public byte LDY() { throw new NotImplementedException(); }
+        public byte PHA() { throw new NotImplementedException(); }
+        public byte ROL() { throw new NotImplementedException(); }
+        public byte SBC() { throw new NotImplementedException(); }
+        public byte STA() { throw new NotImplementedException(); }
+        public byte TAY() { throw new NotImplementedException(); }
+        public byte TYA() { throw new NotImplementedException(); }
+
+        /// <summary>
+        /// Neede for invalid instructions.
+        /// </summary>
+        /// <returns></returns>
+        public byte XXX()
+        {
+            throw new NotImplementedException();
+        }
 
         #endregion
 
@@ -99,6 +194,35 @@ namespace NES_Emulator.Core
         public byte ReadStatusRegister(Flags6502 state)
         {
             return (byte)(Status_Register & (byte)state);
+        }
+
+        /// <summary>
+        /// Map the instruction from the available instruction set 
+        /// to the corresponding implementation.
+        /// </summary>
+        /// <returns></returns>
+        private List<Instruction> MapProcessorInstructions()
+        {
+            List<Instruction> map = new List<Instruction>() {
+                new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7),new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7),
+                new Instruction(Instruction.BPL, BPL, AM_REL, 2), new Instruction(Instruction.BRK, BRK, AM_IMP, 7),new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7),
+                new Instruction(Instruction.JSR, JSR, AM_ABS, 6), new Instruction(Instruction.BRK, BRK, AM_IMP, 7),new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7),
+                new Instruction(Instruction.BMI, BMI, AM_REL, 2), new Instruction(Instruction.BRK, BRK, AM_IMP, 7),new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7),
+                new Instruction(Instruction.RTI, RTI, AM_IMP, 6), new Instruction(Instruction.BRK, BRK, AM_IMP, 7),new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7),
+                new Instruction(Instruction.BVC, BVC, AM_REL, 2), new Instruction(Instruction.BRK, BRK, AM_IMP, 7),new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7),
+                new Instruction(Instruction.RTS, RTS, AM_IMP, 6), new Instruction(Instruction.BRK, BRK, AM_IMP, 7),new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7),
+                new Instruction(Instruction.BVS, BVS, AM_REL, 2), new Instruction(Instruction.BRK, BRK, AM_IMP, 7),new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7),
+                new Instruction(Instruction.UNK, XXX, AM_XXX, 0), new Instruction(Instruction.BRK, BRK, AM_IMP, 7),new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7),
+                new Instruction(Instruction.BCC, BCC, AM_REL, 2), new Instruction(Instruction.BRK, BRK, AM_IMP, 7),new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7),
+                new Instruction(Instruction.LDY, LDY, AM_IMM, 2), new Instruction(Instruction.BRK, BRK, AM_IMP, 7),new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7),
+                new Instruction(Instruction.BCS, BCS, AM_REL, 2), new Instruction(Instruction.BRK, BRK, AM_IMP, 7),new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7),
+                new Instruction(Instruction.CPY, CPY, AM_IMM, 2), new Instruction(Instruction.BRK, BRK, AM_IMP, 7),new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7),
+                new Instruction(Instruction.BNE, BNE, AM_REL, 2), new Instruction(Instruction.BRK, BRK, AM_IMP, 7),new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7),
+                new Instruction(Instruction.CPX, CPX, AM_IMM, 2), new Instruction(Instruction.BRK, BRK, AM_IMP, 7),new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7),
+                new Instruction(Instruction.BEQ, BEQ, AM_REL, 2), new Instruction(Instruction.BRK, BRK, AM_IMP, 7),new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7), new Instruction(Instruction.BRK, BRK, AM_IMP, 7),
+            };
+
+            return map;
         }
     }
 }
--- NES Emulator.Core/Instruction.cs
+++ NES Emulator.Core/Instruction.cs
@@ -10,6 +10,15 @@ namespace NES_Emulator.Core
     public delegate byte AddressMode();
     public class Instruction
     {
+
+        public Instruction(string name, Operate operate, AddressMode addressMode, byte cycle)
+        {
+            Name = name;
+            Operate = operate;
+            AddressMode = addressMode;
+            Cycle = cycle;
+        }
+
         /// <summary>
         /// Name of the instruction from the instruction set.
         /// </summary>
@@ -24,5 +33,68 @@ namespace NES_Emulator.Core
         /// Mechanism to access address from the instruction;
         /// </summary>
         public AddressMode AddressMode { get; set; }
+
+        public byte Cycle { get; set; }
+
+        #region instructions mnemonic
+        public const string UNK = "UNK";
+        public const string ADC = "ADC";
+        public const string AND = "AND";
+        public const string ASL = "ASL";
+        public const string BCC = "BCC";
+        public const string BCS = "BCS";
+        public const string BEQ = "BEQ";
+        public const string BIT = "BIT";
+        public const string BMI = "BMI";
+        public const string BNE = "BNE";
+        public const string BPL = "BPL";
+        public const string BRK = "BRK";
+        public const string BVC = "BVC";
+        public const string BVS = "BVS";
+        public const string CLC = "CLC";
+        public const string CLD = "CLD";
+        public const string CLI = "CLI";
+        public const string CLV = "CLV";
+        public const string CMP = "CMP";
+        public const string CPX = "CPX";
+        public const string CPY = "CPY";
+        public const string DEC = "DEC";
+        public const string DEX = "DEX";
+        public const string DEY = "DEY";
+        public const string EOR = "EOR";
+        public const string INC = "INC";
+        public const string INX = "INX";
+        public const string INY = "INY";
+        public const string JMP = "JMP";
+        public const string JSR = "JSR";
+        public const string LDA = "LDA";
+        public const string LDX = "LDX";
+        public const string LDY = "LDY";
+        public const string LSR = "LSR";
+        public const string NOP = "NOP";
+        public const string ORA = "ORA";
+        public const string PHA = "PHA";
+        public const string PHP = "PHP";
+        public const string PLA = "PLA";
+        public const string PLP = "PLP";
+        public const string ROL = "ROL";
+        public const string ROR = "ROR";
+        public const string RTI = "RTI";
+        public const string RTS = "RTS";
+        public const string SBC = "SBC";
+        public const string SEC = "SEC";
+        public const string SED = "SED";
+        public const string SEI = "SEI";
+        public const string STA = "STA";
+        public const string STX = "STX";
+        public const string STY = "STY";
+        public const string TAX = "TAX";
+        public const string TAY = "TAY";
+        public const string TSX = "TSX";
+        public const string TXA = "TXA";
+        public const string TXS = "TXS";
+        public const string TYA = "TYA";
+
+        #endregion
     }
 }
