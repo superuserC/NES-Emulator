56e6a261b61a36b9548bdd4cf285c431bf4c2196
superuserC <kevin.danthine@gmail.com>
2021-06-28 17:12:04 +0200

    Adding unit test project

---
+++ NES Emulator.Tests/CPU_Test.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NES_Emulator.Tests
+{
+    public class CPU_Test
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+    }
+}
